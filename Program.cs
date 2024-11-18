cd78d02a0de4712b341ca16646008a0d5e2e1385
ManthanThakor <thakormanthan849@gmail.com>
2024-11-18 17:30:57 +0530

    change

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace mediaPlayersystem
+{
+    public interface IMediaPlayer 
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+
+    public class 
+
+    public class AudioPlayer : IMediaPlayer
+    {
+        public string AudioFormat { get; set; }
+
+        public AudioPlayer(string audioFormat)
+        {
+            this.AudioFormat = audioFormat;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"Playing  audio in {AudioFormat} Format");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Audio paused.")
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Video stopped.");
+        }
+    }
+
+    public class VideoPlayer : IMediaPlayer
+    {
+        public string VideoFormat { get; set; }
+
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\"Media PLayer System\"\n");
+
+            Console.ReadLine();
+
+        }
+    }
+}
